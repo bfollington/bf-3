4c900a851623170f3b48729ee40e5c24d6f9ebfb
Ben Follington <5009316+bfollington@users.noreply.github.com>
2021-11-02 10:54:00 +1000

    Add analytics code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
+const env = process.env.NODE_ENV;
+const inDev = env === "development";
+
 class MyDocument extends Document {
   render() {
     return (
@@ -15,6 +18,14 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
             rel="stylesheet"
           />
+          {!inDev && (
+            <script
+              async
+              defer
+              data-website-id="b548cc69-60e7-4439-8ede-a2fcd3338ff0"
+              src="https://google-analytics-sucks.herokuapp.com/umami.js"
+            ></script>
+          )}
         </Head>
         <body>
           <Main />
